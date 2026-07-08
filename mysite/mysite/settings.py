05071d3f1b8e056cfe4ab5473be111e4c160fe46
chenna2000 <chennakesavarao95@gmail.com>
2026-07-08 10:27:12 +0530

    july 8th changes

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -24,6 +24,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # DEBUG = os.getenv("DEBUG") == "True"
 
 
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
